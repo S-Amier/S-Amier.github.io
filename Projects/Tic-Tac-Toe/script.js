13074ed5578fc32f57ed084f00f5741070408754
Shahf <s.a.sd@outlook.com>
2024-07-22 17:35:48 +0200

    Added Tic-Tac-Toe Project
    HTML-CSS finished, for Javascript only need to add the victory screen functions.

---
+++ Projects/Tic-Tac-Toe/script.js
@@ -0,0 +1,44 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('[data-cell]')
+let circleTurn
+
+startGame()
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    //Check for Win
+    // Check for Draw
+    // Switch Turns
+    swapTurns()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
